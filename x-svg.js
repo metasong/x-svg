e0084c08ae99218443a0599133c939a687dd550f
metasong <metaseed@gmail.com>
2022-04-22 14:50:27 -0500

    default slot support

--- x-svg.js
+++ x-svg.js
@@ -7,23 +7,48 @@ customElements.define(
       this.attachShadow({ mode: "open" }).innerHTML = `
         <style>svg{ height:180px }</style>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" width="500" height="500">
-          <circle cx="50%" cy="50%" r="50%" fill="red"></circle>
+          <slot></slot>
           <slot name="foo"></slot>
           <slot name="bar"></slot>
         </svg>
         `;
+
+      // make sure innerHTML is parsed
       setTimeout(() => {
-        // make sure innerHTML is parsed
-        let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svg.innerHTML = this.innerHTML;
-        svg
-          .querySelectorAll("*")
-          .forEach((el) =>
-            this.shadowRoot
-              .querySelector(`slot[name="${el.getAttribute("slot")}"]`)
-              ?.replaceWith(el)
-          );
-          this.replaceChildren(); // remove all children
+        const svg = this.shadowRoot.querySelector("svg");
+        const defaultContent = svg.querySelector("slot:not([name])");
+        // turn everything in lightDOM into SVG (correct SVG NameSpace)
+        let svgChildren = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "svg"
+        );
+        svgChildren.innerHTML = this.innerHTML;
+
+        svgChildren
+          .querySelectorAll("*") // all children
+          .forEach((el) => {
+            const name = el.getAttribute("slot");
+            if (name) {
+              const slot = svg.querySelector(
+                `slot[ name="${name}"]`
+              );
+              if (slot) {
+                slot.replaceWith(el);
+              } else {
+                console.log(
+                  `no slot of name (${name}) configured in template of ${this.tagName}`,
+                  el
+                );
+              }
+            } else {
+              svg.insertBefore(el, defaultContent);
+            }
+          });
+
+          defaultContent.remove(); // remove default slot
+
+        // remove all children in lightDOM
+        this.replaceChildren();
       });
     }
   }
